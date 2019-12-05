0ad65fb87059c187239d56ec435167f663cd35e4
Mads Krogh <DKKRX@dk.ema.ad.pwcinternal.com>
2019-12-05 15:19:27 +0100

    substituted mallocs in recursive call to fix memory leaks

--- mergesort/MergeSort.c
+++ mergesort/MergeSort.c
@@ -3,67 +3,73 @@
 #include <string.h>
 #include <time.h>
 
-int* mergesort(int arr[], int size) {
-    if (size/sizeof(int) == 1) {
-        return arr;
+void mergesort(int arr[], int size, int arrBuf[]) {
+  //end of recursive tree
+  if (size == 1) {
+      arrBuf[0] = arr[0];
+      return;
+  }
+  //half
+  int mid = size/2;
+  int firstSize = mid;
+  int secondSize = size - mid;
+  int first[firstSize];
+  int second[secondSize];
+  for (int i = 0; i < size; i++) {
+    if (i < firstSize) {
+      first[i] = arr[i];
     }
-    int firstSize = (size/sizeof(int)/2)*sizeof(int);
-    int secondSize = ((size/sizeof(int)) - firstSize/sizeof(int))*sizeof(int);
-    //half
-    int* first = malloc(firstSize);
-    int* second = malloc(secondSize);
-    memcpy(first, arr, firstSize);
-    memcpy(second, arr + (firstSize/sizeof(int)), secondSize);
-
-    //sort
-    int* firstSorted = mergesort(first, firstSize);
-    int* secondSorted = mergesort(second, secondSize);
-
-    //merge
-    int i = 0; 
-    int a = 0;
-    int b = 0;
-    int* arrSorted = malloc(size);
-    while (i < size/sizeof(int)) {
-        if ((b == secondSize/sizeof(int) || firstSorted[a] < secondSorted[b]) && a != firstSize/sizeof(int)) {
-            arrSorted[i] = firstSorted[a];
-            ++a;
-        }
-        else if (a == firstSize/sizeof(int) || firstSorted[a] > secondSorted[b]) {
-            arrSorted[i] = secondSorted[b];
-            ++b;
-        }
-         else if (firstSorted[a] == secondSorted[b]) {
-            arrSorted[i] = firstSorted[a];
-            ++a;
-            ++i;
-            arrSorted[i] = secondSorted[b];
-            ++b;
-         }
-        ++i;
+    else {
+      second[i-mid] = arr[i];
     }
-    free(first);
-    free(second);
-    return arrSorted;
+  }
+  //sort
+  int firstArrBuf[firstSize];
+  int secondArrBuf[secondSize];
+  mergesort(first, firstSize, firstArrBuf);
+  mergesort(second, secondSize, secondArrBuf);
+  //merge
+  int i = 0; 
+  int a = 0;
+  int b = 0;
+  while (i < size) {
+      if ((b == secondSize || firstArrBuf[a] < secondArrBuf[b]) && a != firstSize) {
+          arrBuf[i] = firstArrBuf[a];
+          ++a;
+      }
+      else if (a == firstSize || firstArrBuf[a] > secondArrBuf[b]) {
+          arrBuf[i] = secondArrBuf[b];
+          ++b;
+      }
+        else if (firstArrBuf[a] == secondArrBuf[b]) {
+          arrBuf[i] = firstArrBuf[a];
+          ++a;
+          ++i;
+          arrBuf[i] = secondArrBuf[b];
+          ++b;
+        }
+      ++i;
+  }
+  return;
 }
 
 int main() {
-    int elements = 80000;
-    int arr[elements];
-    int i;
-    for (i = 0; i < elements; i++) {
-        arr[i] = rand();
-    }
-    size_t size = sizeof(arr);  
-    time_t time = clock();
-    int* arrSorted = mergesort(arr, size);
-    time = clock() - time;  
-    double elapseTime = ((double)time)/CLOCKS_PER_SEC; // in seconds 
-    printf("Sorted %d elements in %f miliseconds", size/sizeof(int), elapseTime);
+  int arr[100000];
+  size_t size = sizeof(arr)/sizeof(int);  
+  srand(time(0));
+  for (int i = 0; i < size; i++) {
+      arr[i] = rand() % size;
+  }
+  time_t time = clock();
+  int arrSorted[size];
+  mergesort(arr, size, arrSorted);
+  time = clock() - time;  
+  double elapseTime = ((double)time)/CLOCKS_PER_SEC; // in seconds 
+  printf("Sorted %d elements in %f seconds \n", (int) size, elapseTime);
 
-    //print
-    //for (i = 0; i < size/sizeof(int); i++) {
-    //    printf("%d ",arrSorted[i]);
-    //}
-    return 0;
+  //print
+  //for (int i = 0; i < size; i++) {
+  //    printf("%d ",arrSorted[i]);
+  //}
+  return 0;
 }
